b12f1eff1ae6a5f43a68f2cc07aca8e299ed6b66
Caden Rogers <crogers3@my.waketech.edu>
2026-04-29 14:05:50 -0400

    trying to get a shop overlay to work

--- main_script.js
+++ main_script.js
@@ -1,6 +1,7 @@
 // ------------------------------------------------------------------------------//
-// Shortened Variables //
+// Global Variables //
 const main_page = document.getElementById("main_page");
+let rougeulator_screen_value = "";
 // ------------------------------------------------------------------------------//
 
 // ------------------------------------------------------------------------------//
@@ -9,11 +10,11 @@ createMainMenu();
 // ------------------------------------------------------------------------------//
 
 // ------------------------------------------------------------------------------//
-//functions//
+// Functions //
 function createMainMenu() {
     main_page.innerHTML = `
     <div id = "main_menu">
-        <p class = "title">The Rougulator</p>
+        <p class = "title">The Rougeulator</p>
         <button class = "menu_button" id = "start_game_button">Start Game</button>
         <button class = "menu_button" id = "feats_button">Feats</button>
         <button class = "menu_button" id = "exit_game_button">Exit Game</button>
@@ -21,7 +22,7 @@ function createMainMenu() {
     `
     document.getElementById("start_game_button").addEventListener("click", function() {
         destroyMainMenu();
-        createTheRougulator(); // change this to the first level wonce the rougulator is done working.
+        createTheRougeulator(); // change this to the first level once the rougeulator is done working.
     });
     document.getElementById("exit_game_button").addEventListener("click", function() {
         window.close(); // this does not work in most browsers due to security reasons, have to find a way later.
@@ -37,29 +38,98 @@ function destroyMainMenu() {
     main_page.innerHTML = "";
 }
 // =n W n= //
-function createTheRougulator() {
-    main_page.appendChild(document.createElement("div")).id = "the_rougulator";
-    document.getElementById("the_rougulator").innerHTML = `
-    <div class = "rougulator_frame">
+function createTheRougeulator() {
+    main_page.appendChild(document.createElement("div")).id = "the_rougeulator";
+    document.getElementById("the_rougeulator").innerHTML = `
+    <div class = "rougeulator_frame">
         <div class = "display_screen_frame">
             <div class = "display_screen"></div>
+                <p class = "display_screen_text" id="display_screen_text">:</p>
+            <div class = "display_screen_screw"></div>
         </div>
-        <div class = "button_frame">
-            
+        <div class = "essence_frame">
+            <button class = "essence_button" id = "essence_slot_1"></button>
+            <button class = "essence_button" id = "essence_slot_2"></button>
+            <button class = "essence_button" id = "essence_slot_3"></button>
+            <button class = "essence_button" id = "essence_slot_4"></button>
+            <button class = "essence_button" id = "essence_slot_5"></button>
+            <button class = "essence_button" id = "essence_slot_6"></button>
+            <button class = "essence_button" id = "essence_slot_7"></button>
+            <button class = "essence_button" id = "essence_slot_8"></button>
+        </div>
+        <div class = "linking_essence">
+            <button class = "linking_essence_button" id = "linking_essence_slot_1"></button>
+            <button class = "linking_essence_button" id = "linking_essence_slot_2"></button>
+            <button class = "linking_essence_button" id = "linking_essence_slot_3"></button>
+            <button class = "linking_essence_button" id = "linking_essence_slot_4"></button>
+        </div>
+        <div class = "functionality_buttons">
+            <button class = "disintegrate_button" id = "disintegrate_button">Disintegrate</button>
         </div>
     </div>
+    <button id = "debug_essence_button">Debug</button>
     `
+    document.getElementById("debug_essence_button").addEventListener("click", function() {
+        document.getElementById("essence_slot_1").addEventListener("click", function() {
+            essence_one.ability();
+        })
+        document.getElementById("linking_essence_slot_1").addEventListener("click", function() {
+            essence_add.ability();
+        });
+    });
 };
+// =n W n= //
+function debugC() {
+    let calc = document.getElementById("display_screen_text").innerHTML;
+    console.log(eval(rougeulator_screen_value));
+}
 // ------------------------------------------------------------------------------//
 
 // ------------------------------------------------------------------------------//
 // Classes //
 class trialTemplate {
     constructor (title, difficulty, boss) {
-        this.name = title;
+        this.title = title;
         this.difficulty = difficulty;
         this.boss = boss;
     }
 
 }
+// =n W n= //
+class essenceTemplate {
+    constructor (name, ID, effect, value) {
+        this.name = name;
+        this.id = ID;
+        this.effect = effect;
+        this.value = value;
+    }
+}
+// =n W n= //
+class numericalEssence extends essenceTemplate {
+    constructor (name, ID, effect, value) {
+        super(name, ID, effect, value);
+    }
+    ability() {
+        document.getElementById("display_screen_text").innerHTML += this.value;
+        rougeulator_screen_value += this.value;
+    }
+}
+// ------------------------------------------------------------------------------//
+
+// ------------------------------------------------------------------------------//
+// Classes Objects //
+let essence_one = new numericalEssence("1", "essence_1", "", 1);
+let essence_two = new numericalEssence("2", "essence_2", "", 2);
+let essence_three = new numericalEssence("3", "essence_3", "", 3);
+let essence_four = new numericalEssence("4", "essence_4", "", 4);
+let essence_five = new numericalEssence("5", "essence_5", "", 5);
+let essence_six = new numericalEssence("6", "essence_6", "", 6);
+let essence_seven = new numericalEssence("7", "essence_7", "", 7);
+let essence_eight = new numericalEssence("8", "essence_8", "", 8);
+let essence_nine = new numericalEssence("9", "essence_9", "", 9);
+let essence_zero = new numericalEssence("0", "essence_0", "", 0);
+let essence_add = new numericalEssence("+", "essence_add", "", "+");
+let essence_subtract = new numericalEssence("-", "essence_subtract", "", "-");
+let essence_multiply = new numericalEssence("*", "essence_multiply", "", "*");
+let essence_divide = new numericalEssence("/", "essence_divide", "", "/");
 // ------------------------------------------------------------------------------//
